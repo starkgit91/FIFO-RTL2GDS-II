13b8a463863d21e6ef70e09912cb773eb9dbb47d
Prashant Mishra <125981918+starkgit91@users.noreply.github.com>
2026-08-29 00:45:33 +0530

    Fix syntax errors in fifo_full.v module

--- fifo_full.v
+++ fifo_full.v
@@ -1,10 +1,9 @@
 module fifo_full (
     input wire wr_clk, wr_rst_n, wr_en, 
     input reg[6:0] g_rdptr_sync,
-    output reg buff_full;
-    output reg [6:0] b_wrptr;
-    output reg [6:0] g_wrptr;
-
+    output reg buff_full,
+    output reg [6:0] b_wrptr,
+    output reg [6:0] g_wrptr
 );
 
 reg [6:0] b_wrptr_next;
@@ -34,7 +33,7 @@ assign g_wrptr_next = (b_wrptr_next>>1) ^ b_wrptr_next;
 
 // full logic
 assign buff_full_val = (g_wrptr_next=={~g_rdptr_sync[6:5],g_rdptr_sync[4:0]});
-in
+
 always @(posedge wr_clk or negedge wr_rst_n) begin
     if(!wr_rst_n)begin
         buff_full <= 1'b0;
